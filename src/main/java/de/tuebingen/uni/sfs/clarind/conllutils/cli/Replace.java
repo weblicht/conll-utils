fcd2dda4f81957a2c7274e08e28181fa25890abe
Daniël de Kok <me@danieldk.eu>
2013-11-19 13:03:26 +0000

    Replacement now matches regular expressions.

--- src/main/java/de/tuebingen/uni/sfs/clarind/conllutils/cli/Replace.java
+++ src/main/java/de/tuebingen/uni/sfs/clarind/conllutils/cli/Replace.java
@@ -11,8 +11,10 @@ import de.tuebingen.uni.sfs.clarind.conllutils.util.IOUtils;
 import de.tuebingen.uni.sfs.clarind.conllutils.writers.CONLLWriter;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Replace a token, lemma, POS-tag, or dependency relation by another.
@@ -26,7 +28,7 @@ public class Replace {
 
         Layer layer = valueOfLayer(args[1]);
 
-        Map<String, String> replacements = getReplacements(args[0]);
+        Map<Pattern, String> replacements = getReplacements(args[0]);
 
         try (CONLLReader reader = new CONLLReader(IOUtils.openArgOrStdin(args, 2));
              CONLLWriter writer = new CONLLWriter(IOUtils.openArgOrStdout(args, 3))) {
@@ -45,6 +47,9 @@ public class Replace {
                             break;
                         case POSTAG:
                             newToken = replacePOSTag(token, replacements);
+                            break;
+                        case FEATURES:
+                            newToken = replaceFeatures(token, replacements);
                             break;
                         case DEPENDENCY:
                             newToken = replaceDepRel(token, replacements);
@@ -65,25 +70,25 @@ public class Replace {
         }
     }
 
-    private static Map<String, String> getReplacements(String replacementsString) {
-        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+    private static Map<Pattern, String> getReplacements(String replacementsString) {
+        ImmutableMap.Builder<Pattern, String> builder = ImmutableMap.builder();
 
-        for (String replacementString: StringUtils.split(replacementsString, ',')) {
+        for (String replacementString : StringUtils.split(replacementsString, ',')) {
             String[] parts = StringUtils.split(replacementString, ':');
             if (parts.length != 2)
                 throw new IllegalArgumentException(String.format("Replacement should be of the form 'ORIG:NEW': %s", replacementString));
 
-            builder.put(parts[0], parts[1]);
+            builder.put(Pattern.compile(parts[0]), parts[1]);
         }
 
         return builder.build();
     }
 
-    private static CONLLToken replaceDepRel(CONLLToken token, Map<String, String> replacements) {
+    private static CONLLToken replaceDepRel(CONLLToken token, Map<Pattern, String> replacements) {
         if (!token.getDepRel().isPresent())
             return token;
 
-        String replacement = replacements.get(token.getDepRel().get());
+        String replacement = findReplacement(replacements, token.getDepRel().get());
         if (replacement == null)
             return token;
         else
@@ -92,11 +97,11 @@ public class Replace {
                     Optional.of(replacement), token.getPHead(), token.getPDepRel());
     }
 
-    private static CONLLToken replacePOSTag(CONLLToken token, Map<String, String> replacements) {
+    private static CONLLToken replacePOSTag(CONLLToken token, Map<Pattern, String> replacements) {
         if (!token.getPosTag().isPresent())
             return token;
 
-        String replacement = replacements.get(token.getPosTag().get());
+        String replacement = findReplacement(replacements, token.getPosTag().get());
         if (replacement == null)
             return token;
         else
@@ -105,11 +110,11 @@ public class Replace {
                     token.getDepRel(), token.getPHead(), token.getPDepRel());
     }
 
-    private static CONLLToken replaceLemma(CONLLToken token, Map<String, String> replacements) {
+    private static CONLLToken replaceLemma(CONLLToken token, Map<Pattern, String> replacements) {
         if (!token.getLemma().isPresent())
             return token;
 
-        String replacement = replacements.get(token.getLemma().get());
+        String replacement = findReplacement(replacements, token.getLemma().get());
         if (replacement == null)
             return token;
         else
@@ -118,8 +123,8 @@ public class Replace {
                     token.getDepRel(), token.getPHead(), token.getPDepRel());
     }
 
-    private static CONLLToken replaceToken(CONLLToken token, Map<String, String> replacements) {
-        String replacement = replacements.get(token.getForm());
+    private static CONLLToken replaceToken(CONLLToken token, Map<Pattern, String> replacements) {
+        String replacement = findReplacement(replacements, token.getForm());
         if (replacement == null)
             return token;
         else
@@ -128,12 +133,37 @@ public class Replace {
                     token.getPHead(), token.getPDepRel());
     }
 
+    private static CONLLToken replaceFeatures(CONLLToken token, Map<Pattern, String> replacements) {
+        if (!token.getFeatures().isPresent())
+            return token;
+
+        String replacement = findReplacement(replacements, token.getFeatures().get());
+
+        if (replacement == null)
+            return token;
+        else
+            return new CONLLToken(token.getPosition(), token.getForm(), token.getLemma(), token.getCoursePOSTag(),
+                    token.getPosTag(), Optional.of(replacement), token.getHead(), token.getDepRel(),
+                    token.getPHead(), token.getPDepRel());
+    }
+
+    private static String findReplacement(Map<Pattern, String> replacements, String value) {
+        String replacement = null;
+        for (Map.Entry<Pattern, String> e : replacements.entrySet()) {
+            if (e.getKey().matcher(value).matches()) {
+                replacement = e.getValue();
+                break;
+            }
+        }
+        return replacement;
+    }
+
     public static void usage() {
-        System.err.println(String.format("Usage: %s FIND:REPLACE[,FIND:REPLACE]... LAYER [CONLL] [CONLL_OUTPUT]", PROGRAM_NAME));
+        System.err.println(String.format("Usage: %s FIND:REPLACE[,REGEXP:REPLACE]... LAYER [CONLL] [CONLL_OUTPUT]", PROGRAM_NAME));
         System.exit(1);
     }
 
-    private static enum Layer {TOKEN, LEMMA, POSTAG, DEPENDENCY}
+    private static enum Layer {TOKEN, LEMMA, POSTAG, FEATURES, DEPENDENCY}
 
     private static Layer valueOfLayer(String s) {
         return Layer.valueOf(s.toUpperCase());
